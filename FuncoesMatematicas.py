a5f5fca83c69fba6c520df5c38b2cbc2accd391e
Jerônimo <jeronimoaa@al.insper.edu.br>
2022-09-19 14:44:21 -0300

    Derivadas parciais das funcões matemáticas multivariadas implementadas

--- FuncoesMatematicas.py
+++ FuncoesMatematicas.py
@@ -25,13 +25,13 @@ class Funcao1D:
 		self.calcular = funcao
 
 
-	def calcular_derivada(self, var_independente, variacao=1e-6):
+	def calcular_derivada(self, x, variacao=1e-6):
 		'''
 		Calcula numericamente o valor da derivada da função
 		
 		Parâmetros:
-		- var_independente: valor da variável independente
-		- variacao: variação no parâmetro usada para calcular
+		- x: valor da variável independente
+		- variacao: variação na variável em questão usada para calcular
 
 		Retorna um float do valor da derivada
 
@@ -41,8 +41,8 @@ class Funcao1D:
 		derivada = float()
 
 		# cálculo da derivada
-		valor = self.calcular(var_independente)
-		valor_res = self.calcular(var_independente + variacao)
+		valor = self.calcular(x)
+		valor_res = self.calcular(x + variacao)
 		derivada = (valor_res - valor) / variacao
 
 		return derivada
@@ -66,6 +66,54 @@ class Funcao2D:
 		self.calcular = funcao
 
 
+	def calcular_derivada_parcial_x(self, x, y, variacao=1e-6):
+		'''
+		Calcula numericamente o valor da derivada parcial da função em x
+		
+		Parâmetros:
+		- x: valor da variável independente x
+		- y: valor da variável independente y
+		- variacao: variação na variável em questão usada para calcular
+
+		Retorna um float do valor da derivada
+
+		'''
+
+		# valor de retorno
+		derivada = float()
+
+		# cálculo da derivada
+		valor = self.calcular(x, y)
+		valor_res = self.calcular(x + variacao, y)
+		derivada = (valor_res - valor) / variacao
+
+		return derivada
+
+
+	def calcular_derivada_parcial_y(self, x, y, variacao=1e-6):
+		'''
+		Calcula numericamente o valor da derivada parcial da função em y
+		
+		Parâmetros:
+		- x: valor da variável independente x
+		- y: valor da variável independente y
+		- variacao: variação na variável em questão usada para calcular
+
+		Retorna um float do valor da derivada
+
+		'''
+
+		# valor de retorno
+		derivada = float()
+
+		# cálculo da derivada
+		valor = self.calcular(x, y)
+		valor_res = self.calcular(x, y + variacao)
+		derivada = (valor_res - valor) / variacao
+
+		return derivada
+
+
 
 class Funcao3D:
 	''' Classe que define uma função FIXA de três variáveis '''
@@ -82,6 +130,81 @@ class Funcao3D:
 		
 		# define o método de cálculo
 		self.calcular = funcao
+
+
+	def calcular_derivada_parcial_x(self, x, y, z, variacao=1e-6):
+		'''
+		Calcula numericamente o valor da derivada parcial da função em x
+		
+		Parâmetros:
+		- x: valor da variável independente x
+		- y: valor da variável independente y
+		- z: valor da variável independente z
+		- variacao: variação na variável em questão usada para calcular
+
+		Retorna um float do valor da derivada
+
+		'''
+
+		# valor de retorno
+		derivada = float()
+
+		# cálculo da derivada
+		valor = self.calcular(x, y, z)
+		valor_res = self.calcular(x + variacao, y, z)
+		derivada = (valor_res - valor) / variacao
+
+		return derivada
+
+
+	def calcular_derivada_parcial_y(self, x, y, z, variacao=1e-6):
+		'''
+		Calcula numericamente o valor da derivada parcial da função em y
+		
+		Parâmetros:
+		- x: valor da variável independente x
+		- y: valor da variável independente y
+		- z: valor da variável independente z
+		- variacao: variação na variável em questão usada para calcular
+
+		Retorna um float do valor da derivada
+
+		'''
+
+		# valor de retorno
+		derivada = float()
+
+		# cálculo da derivada
+		valor = self.calcular(x, y, z)
+		valor_res = self.calcular(x, y + variacao, z)
+		derivada = (valor_res - valor) / variacao
+
+		return derivada
+
+
+	def calcular_derivada_parcial_z(self, x, y, z, variacao=1e-6):
+		'''
+		Calcula numericamente o valor da derivada parcial da função em z
+		
+		Parâmetros:
+		- x: valor da variável independente x
+		- y: valor da variável independente y
+		- z: valor da variável independente z
+		- variacao: variação na variável em questão usada para calcular
+
+		Retorna um float do valor da derivada
+
+		'''
+
+		# valor de retorno
+		derivada = float()
+
+		# cálculo da derivada
+		valor = self.calcular(x, y, z)
+		valor_res = self.calcular(x, y, z + variacao)
+		derivada = (valor_res - valor) / variacao
+
+		return derivada
 
 
 
